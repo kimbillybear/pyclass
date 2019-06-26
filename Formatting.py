d99da9edab0142b9100e6b7b79913d0abdff8f88
Kimberly Connor <kimbillybear@gmail.com>
2019-06-26 14:14:37 -0400

    fixed Formatting

--- Formatting.py
+++ Formatting.py
@@ -1,1 +1,21 @@
-print(True and True
+import random
+
+name = input("Hello, what is your name?")
+
+print(f"Hello, {name}")
+
+salary = int(input("What is your current salary? (exclude dollar signs and commas)"))
+
+print(f"{name}, your current salary is ${salary}")
+
+raise_per =  random.randint(1,99)
+
+print(f"Your raise is {raise_per}%")
+
+raise_amount = (raise_per * 0.01) * salary
+
+new_salary = salary + ((raise_per * 0.01) * salary)
+
+print(f"Your raise amount is ${raise_amount}")
+
+print(f"{name}, your new salary is ${new_salary}")
